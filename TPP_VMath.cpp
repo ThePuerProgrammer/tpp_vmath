152b6cb61863ae4a1c214089efdbc7067cbbc8be
Jesse Rankins <jwrankins@gmail.com>
2021-09-06 20:41:56 -0500

    inheritence established. magnitude implemented

---
+++ TPP_VMath.cpp
@@ -0,0 +1,63 @@
+// TPP_VMath.cpp
+// written by Jesse Rankins 2021
+#include "TPP_VMath.h"
+#include <cmath>
+#include <xmmintrin.h>
+#include <x86intrin.h>
+#include <vector>
+
+namespace TPP_VMath
+{
+
+    // VECTOR CLASS
+    //========================================================================//
+    float Vector::operator[](int i)
+    {
+        return coordinates[i];
+    }
+
+    float* Vector::getCoordinates()
+    {
+        return coordinates;
+    }
+    //========================================================================//
+
+    // VECTOR3D CLASS
+    //========================================================================//
+    Vector3D::Vector3D()
+    {
+        // default: initialize to the zero vector in R3;
+        Vector3D(0,0,0);
+    }
+
+    Vector3D::Vector3D(float x, float y, float z)
+    {
+        coordinates = new float(3);
+        coordinates[0] = x;
+        coordinates[1] = y;
+        coordinates[2] = z;
+    }
+
+    Vector3D::~Vector3D()
+    {
+        delete [] coordinates;
+    }
+
+    void Vector3D::setCoordinates(float x, float y, float z)
+    {
+        coordinates[0] = x;
+        coordinates[1] = y;
+        coordinates[2] = z;
+    }
+
+    float Vector3D::getMagnitude()
+    {
+        float xSquared = coordinates[0] * coordinates[0];
+        float ySquared = coordinates[1] * coordinates[1];
+        float zSquared = coordinates[2] * coordinates[2];
+        float sumOfSquares = xSquared + ySquared + zSquared;
+        return sqrtf(sumOfSquares);
+    }
+    //========================================================================//
+
+}
---
+++ TPP_VMath.h
@@ -0,0 +1,61 @@
+// TPP_VMath.h
+// written by Jesse Rankins 2021
+#pragma once
+
+namespace TPP_VMath
+{
+    class Vector
+    {
+    public:
+        // PUBLIC FUNCTIONS
+        //--------------------------------------------------------------------//
+        virtual float   getMagnitude() = 0;
+        //--------------------------------------------------------------------//
+
+        // OP OVERLOADS
+        //--------------------------------------------------------------------//
+        float           operator[](int i);
+        //--------------------------------------------------------------------//
+
+        // COORDINATES GETTERS/SETTERS
+        //--------------------------------------------------------------------//
+        float*          getCoordinates();
+        virtual void    setCoordinates(float, float, float) = 0;
+        //--------------------------------------------------------------------//
+
+    protected:
+        // xyz coordinates
+        float* coordinates;
+    };
+
+    class Vector3D : protected Vector
+    {
+    public:
+        // CONSTRUCTORS/DESTRUCTOR
+        //--------------------------------------------------------------------//
+        Vector3D();
+        Vector3D(float, float, float);
+
+        ~Vector3D();
+        //--------------------------------------------------------------------//
+
+        // PUBLIC FUNCTIONS
+        //--------------------------------------------------------------------//
+        float getMagnitude();
+        //--------------------------------------------------------------------//
+
+        // COORDINATES GETTERS/SETTERS
+        //--------------------------------------------------------------------//
+        void  setCoordinates(float, float, float);
+
+        float getX();
+        void  setX();
+
+        float getY();
+        void  setY();
+
+        float getZ();
+        void  setZ();
+        //--------------------------------------------------------------------//
+    };
+};
