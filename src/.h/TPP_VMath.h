2ff02ce460b116197ea55e74ac06e2b20cf22eb3
Jesse Rankins <jwrankins@gmail.com>
2021-09-25 00:45:16 -0500

    mv cmake ./ -VMath_All.h +TPP_VMath.h

---
+++ src/.h/TPP_VMath.h
@@ -0,0 +1,22 @@
+/**
+ * @file                VMath_All.h
+ * @author              Jesse Rankins (https://www.github.com/thepuerprogrammer)
+ * @brief               Makes an include all simple
+ * @version             0.1
+ * @date                2021-09-25
+ * @copyright           Copyright (c) 2021
+ */
+
+#ifndef VMATH_ALL_H
+#define VMATH_ALL_H
+
+#include "Vect.h"
+#include "Vect1D.h"
+#include "Vect2D.h"
+#include "Vect3D.h"
+#include "Vect4D.h"
+#include "VectND.h"
+#include "VWrap.h"
+#include "Matrix.h"
+
+#endif
--- src/.h/VMath_All.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Vect.h"
-#include "Vect1D.h"
-#include "Vect2D.h"
-#include "Vect3D.h"
-#include "Vect4D.h"
-#include "VectND.h"
-#include "VWrap.h"
-#include "Matrix.h"
